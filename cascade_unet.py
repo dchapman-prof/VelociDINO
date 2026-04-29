ff97b6ee83a64e79dfd6e25d90cf426b27acb270
ParniyanFarvardin <parniyanfarvardin@gmail.com>
2026-04-29 08:53:05 -0400

    Tested laplacian autoencoder

--- cascade_unet.py
+++ cascade_unet.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -511,7 +513,7 @@ def LaplacianTranform(x, u_depth=5):
 	# Peform downsampling
 	r = []   # residual maps
 	z = x
-	for u in range(0, u_depth-1):
+	for u in range(0, u_depth):
 		
 		# Calculate residual
 		z_down = F.avg_pool2d(z, kernel_size=2, stride=2, count_include_pad=False)
@@ -568,6 +570,7 @@ class ShuffleConv1x1(nn.Module):
 		x = self.conv1(x)
 		x = channel_shuffle(x, groups=self.groups)
 		x = self.conv2(x)
+		return x
 
 #-----------------------------------------------
 #-----------------------------------------------
@@ -579,7 +582,7 @@ class ShuffleConv1x1(nn.Module):
 class ResidualOverproj(nn.Module):
 	
 	def __init__(self, inC=384, midC=768, overC=768, groups=4):
-		super.__init__()
+		super().__init__()
 		
 		self.conv3x3  = nn.Conv2d(inC, inC, 3, groups=inC, padding='same')
 		self.overproj = ShuffleConv1x1(inC,midC,overC,groups)
@@ -594,7 +597,7 @@ class ResidualOverproj(nn.Module):
 def BlowUpChannels(x, overscale):
 	N,C,H,W = x.shape
 	x = torch.reshape(x, (N, 1, C, H, W))
-	x = torch.repeat(x, (1,overscale,1,1,1))
+	x = x.repeat((1,overscale,1,1,1))
 	x = torch.reshape(x, (N,overscale*C,H,W)).contiguous()
 	return x
 
@@ -617,7 +620,7 @@ def BlowDownChannels(x, overscale):
 class Encoder(nn.Module):
 	
 	def __init__(self, inC=384, baseC=16, C_factor=2, u_depth=5, groups=4, overscale=2):
-		super.__init__()
+		super().__init__()
 		self.inC = inC
 		self.baseC = baseC
 		self.u_depth = u_depth
@@ -632,12 +635,12 @@ class Encoder(nn.Module):
 		overC = inC * overscale
 		outC = baseC
 		for u in range(u_depth):
-	
+
 			# Convert inC into overscale   project 384->768 with 3x3 conv
 			self.scale_weights.append(  ResidualOverproj(inC,overC,overC,groups)  )   
 
 			# Calculate basis dimensions  768 -> 16 (depth dependent)
-			self.downproject.append(  ShuffleConv1x1(overC, inC, outC, groups)  )
+			self.downproj.append(  ShuffleConv1x1(overC, inC, outC, groups)  )
 			
 			outC *= C_factor
 	
@@ -649,21 +652,21 @@ class Encoder(nn.Module):
 		# Start at the end of the pyramid
 		outC = self.baseC
 		for u in range(self.u_depth-1):
-			outC *= C_factor
+			outC *= self.C_factor
 
 		# Make an empty pyramid
 		enc_pyr = []
-		for u in range(self.u_depth):
+		for u in range(self.u_depth+1):
 			enc_pyr.append( None )
 
 		
 		# Perform the inverse pyramid
-		z_down = x[self.u_depth-1]
-		enc_pyr[self.u_depth-1] = z_down
-		for u in range(self.u_depth-2, -1, -1):
-		
+		z_down = x[self.u_depth]
+		enc_pyr[self.u_depth] = z_down
+		for u in range(self.u_depth-1, -1, -1):
+
 			# Calculate scaled weights
-			weights = self.scale_weights(z_down)
+			weights = self.scale_weights[u](z_down)
 			weights = F.sigmoid(weights)
 			weights = F.interpolate(weights, scale_factor=2.0, mode='bilinear', align_corners=False)
 			
@@ -674,7 +677,7 @@ class Encoder(nn.Module):
 			r = r*weights
 			
 			# Downproject channels  768 -> 16
-			e = self.downproject(e)
+			e = self.downproj[u](r)
 			
 			# Put into pyramid
 			enc_pyr[u] = e
@@ -700,7 +703,7 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
 	
 	def __init__(self, inC=384, baseC=16, C_factor=2, u_depth=5, groups=4, overscale=2):
-		super.__init__()
+		super().__init__()
 		self.inC = inC
 		self.baseC = baseC
 		self.u_depth = u_depth
@@ -708,7 +711,7 @@ class Decoder(nn.Module):
 		self.overscale = overscale
 		self.C_factor = C_factor
 		
-		self.upproj      = nn.ModuleList()
+		self.upproj        = nn.ModuleList()
 		self.scale_weights = nn.ModuleList()
 		
 		# For each layer in u_depth
@@ -720,7 +723,7 @@ class Decoder(nn.Module):
 			self.scale_weights.append(  ResidualOverproj(inC,overC,overC,groups)  )   
 
 			# Calculate basis dimensions  16 -> 768 (depth dependent)
-			self.upproject.append(  ShuffleConv1x1(outC, inC, overC, groups)  )
+			self.upproj.append(  ShuffleConv1x1(outC, inC, overC, groups)  )
 			
 			outC *= C_factor
 	
@@ -732,26 +735,26 @@ class Decoder(nn.Module):
 		# Start at the end of the pyramid
 		outC = self.baseC
 		for u in range(self.u_depth-1):
-			outC *= C_factor
+			outC *= self.C_factor
 
 		# Make an empty pyramid
 		dec_pyr = []
-		for u in range(self.u_depth):
+		for u in range(self.u_depth+1):
 			dec_pyr.append( None )
 
 		
 		# Perform the inverse pyramid
-		z_down = e[self.u_depth-1]
-		dec_pyr[self.u_depth-1] = z_down
-		for u in range(self.u_depth-2, -1, -1):
+		z_down = e[self.u_depth]
+		dec_pyr[self.u_depth] = z_down
+		for u in range(self.u_depth-1, -1, -1):
 		
 			# Calculate scaled weights
-			weights = self.scale_weights(z_down)
+			weights = self.scale_weights[u](z_down)
 			weights = F.sigmoid(weights)
 			weights = F.interpolate(weights, scale_factor=2.0, mode='bilinear', align_corners=False)
 			
 			# Upproject channels  16 -> 768
-			r = self.upproject(x[u])
+			r = self.upproj[u](e[u])
 		
 			# Multiply by scaled weights
 			r = r*weights
@@ -768,10 +771,248 @@ class Decoder(nn.Module):
 		
 		return dec_pyr
 
+
+
+
+def mkdir(path):
+	try:
+		os.makedirs(path, exist_ok=True)
+	except Exception as e:
+		print("Could not create directory:", path, e)
+
+
 def main():
+	import time
+	import blockreader_sa1b
+	import bicubic
+	
+	print('---------------------------------------------')
+	print(' Initialize Autoencoder')
+	print('---------------------------------------------')
+
+	device = 'cuda'
+
+	encoder = Encoder().to(device)
+	decoder = Decoder().to(device)
+
+
+	
+	print('---------------------------------------------')
+	print(' Initialize Blockreader')
+	print('---------------------------------------------')
+	batch_size = 64
+	dataset_folder = "/nvme0/sa1b"
+	print(f"Initializing dataset from: {dataset_folder}")
+
+	# Initialize the dataset
+	# Note: Using your folder-based __init__ requirement
+	reader = blockreader_sa1b.SA1B_DINO_blockreader(dataset_folder, batch_size)
+	n_img   = reader.len()
+	#n_img   = 150
+	#n_batch = (n_img+batch_size-1) // batch_size
+	n_batch = n_img // batch_size    # NO partial batches
+	n_epoch = 90
+
+	print(f"N images: {n_img}")
+	print(f"N batches: {n_batch}")
+	print(f"Batch size: {batch_size}")
+	print("-" * 50)
+
+	start_time = time.time()
+	last_batch_time = start_time
+
+
+	
+	print('---------------------------------------------')
+	print(' Initialize Bicubic')
+	print('---------------------------------------------')
+	bcb = bicubic.Bicubic(
+	  in_shape=(batch_size,1500,1500,4),
+	  img_shape=(batch_size,896,896,4),
+	  mask_shape=(batch_size,64,64,384),
+	  img_mean=(0.485, 0.456, 0.406),
+	  img_std=(0.229, 0.224, 0.225),
+	  device='cuda')
+
+
+
+	print('---------------------------------------------')
+	print(' Optimizer')
+	print('---------------------------------------------')
+	parameters = list(encoder.parameters()) + list(decoder.parameters())
+	optimizer = torch.optim.AdamW(
+		parameters,
+		lr= 0.01,
+		#lr = lr,
+		#weight_decay=0.05
+		weight_decay=0.02)
+	scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.75)
+
+	
+	encoder_params = sum(p.numel() for p in encoder.parameters())
+	decoder_params = sum(p.numel() for p in decoder.parameters())
+	print(f"Encoder parameters: {encoder_params}")
+	print(f"Decoder parameters: {decoder_params}")
+	input('enter')
+
+	print('---------------------------------------------')
+	print(' Make output folders')
+	print('---------------------------------------------')
+	outdir = 'autoencoder'
+	mkdir(outdir)
+	
+
+	floss = open(outdir+'/loss.csv', 'w')
+
+	print('---------------------------------------------')
+	print(' For every epoch')
+	print('---------------------------------------------')
+	for epoch in range(n_epoch):
+
+		# reset losses per epoch
+		epoch_total_loss = 0.0
+		epoch_layer_loss = []
+		for u in range(6):
+			epoch_layer_loss.append(0.0)
+
+	
+		print('-----------------')
+		print(' For every batch')
+		print('-----------------')
+		# Iterate through the dataloader
+		for i in range(n_batch):
+
+			#----------------
+			# Read the images/features from lmdb
+			#----------------
+			images, features_np = reader.read_batch()
+
+			features = torch.tensor(features_np, dtype=torch.uint8, device='cuda', requires_grad=False)
+
+			# Restore the features using bicubic
+			
+			# Convert from [N,H,W,C] to [N,C,H,W]
+			with torch.no_grad():
+				bcb.restore_uint8_features(features)
+				features = bcb.mask_data_raw
+				features = torch.permute(features, (0,3,1,2)).contiguous()
+			
+			
+			#----------------
+			# Run Autoencoder
+			#----------------
+
+			with torch.no_grad():
+				features_laplace = LaplacianTranform(features)
+
+			#for i in range(len(features_laplace)):
+			#	print('features_laplace', i, features_laplace[i].shape)
+			optimizer.zero_grad()
+
+
+			features_enc = encoder(features_laplace)
+
+			#for i in range(len(features_enc)):
+			#	print('features_enc', i, features_enc[i].shape)
+		
+			features_dec = decoder(features_enc)
+
+			#for i in range(len(features_dec)):
+			#	print('features_dec', i, features_dec[i].shape)
+
+			# Calculate the layer loss
+			total_loss = 0.0
+			layer_loss = []
+			for u in range(len(features_dec)):
+				pred = features_dec[u]
+				obs  = features_laplace[u]
+				
+				# mse loss
+				loss = pred-obs
+				loss = torch.mean(loss*loss)
+				layer_loss.append(loss)
+				total_loss = loss + total_loss
+				print('layer', u, 'loss', float(loss))
+			
+			total_loss.backward()
+			optimizer.step()
+			print('total_loss', float(total_loss))
+
+			# append epoch losses
+			epoch_total_loss = total_loss + epoch_total_loss
+			for u in range(len(layer_loss)):
+				epoch_layer_loss[u] = layer_loss[u] + epoch_layer_loss[u]
+
+			#----------------
+			# Print timing info
+			#----------------
+			current_time = time.time()
+			batch_duration = current_time - last_batch_time
+			total_elapsed = current_time - start_time
+
+			print(f"Batch {i+1}/{n_batch}")
+			print(f"  Images:   Shape {images.shape} | Dtype {images.dtype}")
+			print(f"  Features: Shape {features.shape} | Dtype {features.dtype}  |  Type {type(features)}")
+			print(f"  Timing:   Batch: {batch_duration:.4f}s | Total: {total_elapsed:.4f}s")
+			print("-" * 30, flush=True)
+
+			last_batch_time = current_time
+
+		print('-----------------')
+		print(' Epoch', epoch, 'loss')
+		print('-----------------')
+
+		# Divide by number of batches
+		epoch_total_loss /= n_batch
+		for u in range(len(layer_loss)):
+			epoch_layer_loss[u] /= n_batch
+
+		# write the loss
+		floss.write('%.1f\t%d\t%.6f\t' % (total_elapsed, epoch, float(epoch_total_loss)))
+		for u in range(len(epoch_layer_loss)):
+			floss.write('%.6f\t' % float(epoch_layer_loss[u]))
+		floss.write('\n')
+		floss.flush()
+		
+		print('%d\t%.6f\t' % (epoch, float(epoch_total_loss)), end='')
+		for u in range(len(epoch_layer_loss)):
+			print('%.6f\t' % float(epoch_layer_loss[u]), end='')
+		print('', flush=True)
+
+		print('-----------------')
+		print('Save the weights')
+		print('-----------------')
+
+		encoder_path = '%s/encoder_%05d.pth' % (outdir, epoch)
+		print(encoder_path)
+		torch.save(encoder.state_dict(), encoder_path)
+
+		decoder_path = '%s/decoder_%05d.pth' % (outdir, epoch)
+		print(decoder_path)
+		torch.save(decoder.state_dict(), decoder_path)
+
+	floss.close()
+
+
+	final_time = time.time()
+	avg_speed = (n_batch * batch_size) / (final_time - start_time)
+
+	print("\nTest Complete!")
+	print(f"Total time for {n_batch} batches: {final_time - start_time:.2f}s")
+	print(f"Average throughput: {avg_speed:.2f} images/sec")
 
 	print('Success!')
 		
 if __name__ == "__main__":
 	main()
 		
+		
+		# Debugging printing out raw values
+		#idx = 0
+		#for y in range(64):
+		#	for x in range(64):
+		#		print('%d ' % features_np[0, 12 + 384*2 + 384*2 + idx], end='')
+		#		idx+=384
+		#	print('')
+
+
